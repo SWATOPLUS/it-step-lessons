c2e9143831f8d73512e20e8a3cec6f7f950b7b70
Pavel Zhdanovich <SWATOPLUS@gmail.com>
2023-02-09 19:06:18 +0300

    Initial commin

---
+++ RezorePagesSample/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Net;
+
+namespace RezorePagesSample.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        private const string EmailDomain = "example.com";
+
+        public string? MailTo { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            if (string.IsNullOrWhiteSpace(MailTo))
+            {
+                var content = await output.GetChildContentAsync();
+                var address = content.GetContent() + "@" + EmailDomain;
+                output.Attributes.SetAttribute("href", "mailto:" + address);
+                output.Content.SetContent(address);
+            }
+            else
+            {
+                var address = MailTo + "@" + EmailDomain;
+                output.Attributes.SetAttribute("href", "mailto:" + address);
+            }
+
+            output.Attributes.SetAttribute("style", "color: blue; display: block;");            
+        }
+    }
+}
